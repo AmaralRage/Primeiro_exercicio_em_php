e0431b42942a7142d137373b758d7540ad090ab0
Lucas Amaral Cruz Aragão <82917923+AmaralRage@users.noreply.github.com>
2021-08-18 19:46:17 -0300

    Create exercicio.php

---
+++ exercicio.php
@@ -0,0 +1,17 @@
+<?php
+$nome_usuario = "usuario";
+$numero = 7;
+$logica = true;
+$valor = 1.90;
+$nome = "variavel";
+echo "$nome_usuario <br>";
+echo "$numero <br>";
+echo "$logica <br>";
+echo "$valor <br>";
+echo "$nome <br>";
+$$nome = "NaoTenhoValor";
+echo "$variavel <br>";
+$$nome_usuario = "Amaral";
+echo "$usuario <br>";
+echo "3*$numero <br>";
+echo 3*$numero;
